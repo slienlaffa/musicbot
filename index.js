91c93000b7339b8aa25f4474590f66f4e8b548a1
Sergio Lienlaff Aguirre <s.lienlaff.a@gmail.com>
2021-10-29 04:22:09 -0300

    add react emojis and remove song changed
    add react emoji as a response to the execution of commands
    remove song changed event because if there is an error at getting the song, will crash the bot

--- index.js
+++ index.js
@@ -1,186 +1,236 @@
-const { Client, Intents } = require("discord.js");
+const { Client, Intents } = require('discord.js')
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]
-});
-const { token } = require('./config.json');
-const prefix= '-';
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    ],
+})
+const { token } = require('./config.json')
+const prefix = '-'
 
-const { Player } = require("discord-music-player");
+const { Player } = require('discord-music-player')
 const player = new Player(client, {
     leaveOnEnd: false, // This options are optional.
     volume: 50,
-    quality: 'low'
-});
+    quality: 'low',
+})
 // You can define the Player as *client.player* to easly access it.
-client.player = player;
+client.player = player
 
-client.on("ready", () => {
-    console.log("I am ready to Play with DMP 🎶");
-});
+client.on('ready', () => {
+    console.log('I am ready to Play with DMP 🎶')
+})
 
-client.login(token);
+client.login(token)
 
-client.on("error", (e) => console.error(e));
-client.on("warn", (e) => console.warn(e));
+client.on('error', (e) => console.error(e))
+client.on('warn', (e) => console.warn(e))
 
 // Init the event listener only once (at the top of your code).
 client.player
     // Emitted when channel was empty.
-    .on('channelEmpty',  (queue) =>
-        queue.data.message.channel.send(`Everyone left the Voice Channel, queue ended.`))
+    .on('channelEmpty', (queue) =>
+        queue.data.message.channel.send(
+            `Everyone left the Voice Channel, queue ended.`
+        )
+    )
     // Emitted when a song was added to the queue.
- //   .on('songAdd',  (queue, song) =>
- //       queue.data.message.channel.send(`Song ${song} was added to the queue.`))
+    //   .on('songAdd',  (queue, song) =>
+    //       queue.data.message.channel.send(`Song ${song} was added to the queue.`))
     // Emitted when a playlist was added to the queue.
-    .on('playlistAdd',  (queue, playlist) =>
-        queue.data.message.channel.send(`Playlist ${playlist} with ${playlist.songs.length} was added to the queue.`))
+    .on('playlistAdd', (queue, playlist) =>
+        queue.data.message.channel.send(
+            `Playlist ${playlist} with ${playlist.songs.length} was added to the queue.`
+        )
+    )
     // Emitted when there was no more music to play.
-    .on('queueEnd',  (queue) =>
-        queue.data.message.channel.send(`The queue has ended.`))
+    .on('queueEnd', (queue) =>
+        queue.data.message.channel.send(`The queue has ended.`)
+    )
     // Emitted when a song changed.
-    .on('songChanged', (queue, newSong, oldSong) =>
-        queue.data.message.channel.send(`${newSong} is now playing.`))
+    /*   .on('songChanged', (queue, newSong, oldSong) =>
+        queue.data.message.channel.send(`${newSong} is now playing.`)
+        // when 403 error, there is no song to get name
+    )*/
     // Emitted when a first song in the queue started playing.
-    .on('songFirst',  (queue, song) =>
-        queue.data.message.channel.send(`Started playing ${song}.`))
+    .on('songFirst', (queue, song) =>
+        queue.data.message.channel.send(
+            `Started playing ${song}. ${client.emojis.cache.get(
+                '903523328520769588'
+            )}`
+        )
+    )
     // Emitted when someone disconnected the bot from the channel.
     .on('clientDisconnect', (queue) =>
-        queue.data.message.channel.send(`I was kicked from the Voice Channel, queue ended.`))
+        queue.data.message.channel.send(
+            `I was kicked from the Voice Channel, queue ended.`
+        )
+    )
     // Emitted when deafenOnJoin is true and the bot was undeafened
     .on('clientUndeafen', (queue) =>
-        queue.data.message.channel.send(`I got undefeanded.`))
+        queue.data.message.channel.send(`I got undefeanded.`)
+    )
     // Emitted when there was an error in runtime
     .on('error', (error, queue) => {
-        console.log(`Error: ${error} in ${queue.guild.name}`);
-    });
+        logError(error, queue)
+    })
 
-const { RepeatMode } = require('discord-music-player');
+const { RepeatMode } = require('discord-music-player')
 
 client.on('messageCreate', async (message) => {
-    if(message.author.bot) return;
-    if(!message.content.startsWith(prefix)) return
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+    if (message.author.bot) return
+    if (!message.content.startsWith(prefix)) return
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase()
 
-    
-    if(command === 'help' || command === 'h') {
-        message.channel.send('-play -playlist -skip -stop -loop -unloop -loopQueue -clear -shuffle -queue -pause -resume')
-    }
-    
-    if(!message.member.voice.channel) return
-    
-    let guildQueue = client.player.getQueue(message.guild.id);
-
-    if(command === 'join' || command === 'j') {
-        let queue = client.player.createQueue(message.guild.id, { data: { message } });
-        await queue.join(message.member.voice.channel);
+    if (command === 'help' || command === 'h') {
+        message.channel.send(
+            '-play -playlist -skip -stop -loop -unloop -loopQueue -clear -shuffle -queue -pause -resume -nowplaying -remove'
+        )
     }
 
-    if(command === 'play' || command === 'p') {
-        let queue = client.player.createQueue(message.guild.id, { data: { message } });
-        await queue.join(message.member.voice.channel);
-        let functionPlay = /youtube.com\/.*list/.test(args[0]) ? queue.playlist : queue.play
-        let song = await functionPlay.call(queue, args.join(' ')).catch(err => {
-            console.log(new Date(Date.now()).toLocaleTimeString('en-US', {timezone: 'Zone America/Santiago'}) + ' Error: ' +  err)
+    if (!message.member.voice.channel) return
+
+    let guildQueue = client.player.getQueue(message.guild.id)
+
+    if (command === 'join' || command === 'j') {
+        let queue = client.player.createQueue(message.guild.id, {
+            data: { message },
+        })
+        await queue.join(message.member.voice.channel)
+    }
+
+    if (command === 'play' || command === 'p') {
+        let queue = client.player.createQueue(message.guild.id, {
+            data: { message },
+        })
+        await queue.join(message.member.voice.channel)
+        let functionPlay =
+            /youtube.com\/.*list/.test(args[0]) ||
+            /spotify.com\/playlist/.test(args[0])
+                ? queue.playlist
+                : queue.play
+        await functionPlay.call(queue, args.join(' ')).catch((error) => {
+            logError(error, queue)
             message.channel.send('No pregunti por qué, pero la wea no funciona')
-            if(!guildQueue)
-                queue.stop();
-        });
+            if (!guildQueue) queue.stop()
+        })
     }
 
-    if(command === 'playlist' || command === 'pl') {
-        let queue = client.player.createQueue(message.guild.id, { data: { message } });
-        await queue.join(message.member.voice.channel);
-        let song = await queue.playlist(args.join(' ')).catch(err => {
-            console.log(new Date(Date.now()).toLocaleTimeString('en-US', {timezone: 'Zone America/Santiago'}) + ' Error: ' +  err)
+    // may delete, if play can identify playlist correctly
+    if (command === 'playlist' || command === 'pl') {
+        let queue = client.player.createQueue(message.guild.id, {
+            data: { message },
+        })
+        await queue.join(message.member.voice.channel)
+        await queue.playlist(args.join(' ')).catch((error) => {
+            logError(error, queue)
             message.channel.send('No pregunti por qué, pero la wea no funciona')
-            if(!guildQueue)
-                queue.stop();
-        });
+            if (!guildQueue) queue.stop()
+        })
     }
 
     // If the guildQueue does not exist, any of the other commands will fail
-    if(!guildQueue) return
+    if (!guildQueue) return
 
-    if(command === 'skip' || command === 's') {
-        if(guildQueue.songs.length == 0) return
-        guildQueue.skip();
-        message.channel.send('Canción saltada')
+    if (command === 'skip' || command === 's') {
+        if (guildQueue.songs.length == 0) return
+        guildQueue.skip()
+        message.react('⏩')
     }
 
-    if(command === 'stop') {
-        guildQueue.setPaused(true);
-        message.channel.send('¿Cual es la diferencia con pausado?')
+    if (command === 'stop') {
+        guildQueue.setPaused(true)
+        message.react('⏹️')
     }
 
-    if(command === 'leave' || command === 'l') {
-        guildQueue.stop();
+    if (command === 'leave' || command === 'l') {
+        guildQueue.stop()
     }
 
-    if(command === 'unloop') {
-        guildQueue.setRepeatMode(RepeatMode.DISABLED); // or 0 instead of RepeatMode.DISABLED
-        message.channel.send('Loop desactivado')
+    if (command === 'unloop') {
+        guildQueue.setRepeatMode(RepeatMode.DISABLED) // or 0 instead of RepeatMode.DISABLED
+        message.react('➡️')
     }
 
-    if(command === 'loop') {
-        guildQueue.setRepeatMode(RepeatMode.SONG); // or 1 instead of RepeatMode.SONG
-        message.channel.send('Loop activado')
+    if (command === 'loop') {
+        guildQueue.setRepeatMode(RepeatMode.SONG) // or 1 instead of RepeatMode.SONG
+        message.react('🔁')
     }
 
-    if(command === 'loopqueue') {
-        guildQueue.setRepeatMode(RepeatMode.QUEUE); // or 2 instead of RepeatMode.QUEUE
-        message.channel.send('Loop de queue activado')
+    if (command === 'loopqueue') {
+        guildQueue.setRepeatMode(RepeatMode.QUEUE) // or 2 instead of RepeatMode.QUEUE
+        message.react('🔂')
     }
 
-    if(command === 'setvolume') {
-        guildQueue.setVolume(parseInt(args[0]));
+    if (command === 'setvolume') {
+        guildQueue.setVolume(parseInt(args[0]))
     }
 
-    if(command === 'seek') {
-        guildQueue.seek(parseInt(args[0]) * 1000);
+    if (command === 'seek') {
+        guildQueue.seek(parseInt(args[0]) * 1000)
     }
 
-    if(command === 'clear') {
-        guildQueue.clearQueue();
+    if (command === 'clear') {
+        guildQueue.clearQueue()
         message.channel.send('Queue limpiada')
     }
 
-    if(command === 'shuffle') {
-        guildQueue.shuffle();
-        message.channel.send('Shuffleando')
+    if (command === 'shuffle') {
+        guildQueue.shuffle()
+        message.react('🔀')
     }
 
-    if(command === 'queue' || command === 'q') {
-        let queuePretty = guildQueue.songs.slice(0,9)
-        .map((song, index) => `${index+1}- ${song.name}\n`).join('\n')
-        message.channel.send('\`\`\`' + queuePretty + '\`\`\`')
+    if (command === 'queue' || command === 'q') {
+        let queuePretty = guildQueue.songs
+            .slice(0, 9)
+            .map((song, index) => `${index + 1}- ${song.name}\n`)
+            .join('\n')
+        message.channel.send('```' + queuePretty + '```')
     }
 
-    if(command === 'getvolume') {
+    if (command === 'getvolume') {
         message.channel.send(guildQueue.volume)
     }
 
-    if(command === 'nowplaying') {
-        console.log(`Now playing: ${guildQueue.nowPlaying}`);
+    if (command === 'nowplaying') {
+        const ProgressBar = guildQueue.createProgressBar()
+        message.channel.send(
+            `Now playing: ${guildQueue.nowPlaying}\n${ProgressBar.prettier}`
+        )
     }
 
-    if(command === 'pause') {
-        guildQueue.setPaused(true);
-        message.channel.send('Pausado')
+    if (command === 'pause') {
+        guildQueue.setPaused(true)
+        message.react('⏸️')
     }
 
-    if(command === 'resume' || command === 'r') {
-        guildQueue.setPaused(false);
+    if (command === 'resume' || command === 'r') {
+        guildQueue.setPaused(false)
+        message.react('▶️')
     }
 
-    if(command === 'remove') {
-        guildQueue.remove(parseInt(args[0]));
+    if (command === 'remove') {
+        guildQueue.remove(parseInt(args[0]))
+        message.react('513547150697955338')
     }
 
-    if(command === 'createprogressbar') {
-        const ProgressBar = guildQueue.createProgressBar();
-        
+    if (command === 'createprogressbar') {
+        const ProgressBar = guildQueue.createProgressBar()
+
         // [======>              ][00:35/2:20]
-        console.log(ProgressBar.prettier);
+        console.log(ProgressBar.prettier)
     }
 })
+
+function logError(error, queue) {
+    console.log(timeNow() + 'En: ' + queue.guild.name + ' Error: ' + error)
+}
+
+function timeNow() {
+    return new Date(Date.now()).toLocaleTimeString('en-GB', {
+        timezone: 'America/Santiago',
+    })
+}
